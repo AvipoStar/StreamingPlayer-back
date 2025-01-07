30ada6ac4ec406dd513b66c737196ee5f469e243
FedorovKD <FedorovKD>
2025-01-07 15:04:03 +0700

    Добавил апи получение трэков по жанру

--- controllers/genre.py
+++ controllers/genre.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI, HTTPException, status
 
 from config.Database import get_connection
@@ -13,14 +15,78 @@ async def getGenres():
 
     cursor = await connection.cursor()
     try:
-        await cursor.execute("SELECT id, name FROM genres WHERE type = 'music'")
+        await cursor.execute("SELECT id, name, color FROM genres WHERE type = 'music'")
         result = await cursor.fetchall()
         genres = []
 
         for genre in result:
-            genres.append({"id": genre[0], "name": genre[1]})
+            genres.append({"id": genre[0], "name": genre[1], "color": genre[2]})
 
         return genres
+    except Error as e:
+        print(f"Error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Ошибка получения жанров"
+        )
+    finally:
+        await cursor.close()
+        connection.close()
+
+
+async def getGenreTracks(genre_id: int, user_id: int):
+    connection = await get_connection()
+    if connection is None:
+        return
+
+    cursor = await connection.cursor()
+    try:
+        await  cursor.execute("""
+        SELECT name, color FROM genres 
+        WHERE id = %s""", (genre_id,))
+        genre = await cursor.fetchone()
+
+        genre_name, genre_color = genre
+
+        await cursor.execute(""" 
+        SELECT
+            mi.id,
+            mi.title,
+            mi.duration,
+            mi.file_url,
+            a.preview_url,
+            JSON_ARRAYAGG(
+                JSON_OBJECT('id', au.user_id, 'nickname', u.nickname)
+            ) AS authors,
+            CASE
+                WHEN f.media_item_id IS NOT NULL THEN TRUE
+                ELSE FALSE
+            END AS in_favorite
+        FROM media_items mi
+        JOIN albums a ON a.id = mi.album_id
+        JOIN authors au ON au.album_id = a.id
+        JOIN users u ON u.id = au.user_id
+        JOIN mediaItem_genre ig ON mi.id = ig.id_mediaItem
+        LEFT JOIN favorites f ON mi.id = f.media_item_id AND f.user_id = %s
+        WHERE ig.id_genre = %s
+        GROUP BY mi.id, mi.title, mi.duration, mi.file_url, a.preview_url;""", (user_id, genre_id))
+        result = await cursor.fetchall()
+
+        tracks = []
+        for track in result:
+            track_id, title, duration, file_url, preview_url, authors, in_favorite = track
+            tracks.append({
+                "id": track_id,
+                "title": title,
+                "duration": duration,
+                "preview_url": preview_url,
+                "file_url": file_url,
+                "inFavorites": in_favorite,
+                "authors": json.loads(authors) if authors else []
+            })
+
+        return {"genre": {"id": genre_id, "name": genre_name, "color": genre_color},
+                "tracks": tracks}
     except Error as e:
         print(f"Error: {e}")
         raise HTTPException(
--- routers/genre.py
+++ routers/genre.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
-from controllers.genre import getGenres
+from fastapi import APIRouter, Depends
+
+from config.get_user_from_token import get_user_id_from_token
+from controllers.genre import getGenres, getGenreTracks
 
 router = APIRouter()
 
@@ -7,4 +9,10 @@ router = APIRouter()
 @router.get('/', tags=["Genre"])
 async def get_genres():
     result = await getGenres()
+    return result
+
+
+@router.get('/{genre_id}', tags=["Genre"])
+async def get_genres(genre_id: int, user_id: int = Depends(get_user_id_from_token)):
+    result = await getGenreTracks(genre_id, user_id)
     return result
